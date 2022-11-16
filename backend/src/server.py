949ae9c95759c56e86284f3a46b1f742d9e862a8
dekellevy99 <dekelllevy@gmail.com>
2022-11-16 17:21:35 +0200

    import the router of the users endpoints

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.transactions import transactions_routers
+from routers.users import users_routers
+
 import uvicorn
 
 app = FastAPI()
 app.include_router(transactions_routers.router)
+app.include_router(users_routers.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():
